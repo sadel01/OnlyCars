cdf731b9f23935fd786881d689b64fdb8ce02402
Nelson Silva <nelsonsjf@gmail.com>
2024-04-04 16:22:27 -0300

    Avance: Creación de archivos de vistas y enrutamiento.  Tiempo: 35min

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,10 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/catalog">Catalog</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+        <RouterLink to="/publicacion"></RouterLink>
+        <RouterLink to="/vender">Vender</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/SellView.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <h1>User</h1>
+    </main>
+</template>
