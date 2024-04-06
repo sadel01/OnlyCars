c3cfc5383933ae9993223bc82899729b5e220728
Nelson Silva <nelsonsjf@gmail.com>
2024-04-06 18:11:23 -0300

    Avance navbar y pagina principal. Tiempo 1:30hrs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HeroSection from '../components/HeroSection.vue'
+import SellSectionHome from '../components/SellSectionHome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HeroSection />
+    <SellSectionHome/>
   </main>
 </template>
