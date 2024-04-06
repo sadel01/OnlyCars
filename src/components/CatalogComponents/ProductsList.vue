db23877befff24a11f6db8ff23c6de191c59b95e
BorgTb <ameza21@alumnos.utalca.cl>
2024-04-06 20:17:08 -0300

    crear carpeta para el componente CatalogComponents hacer la lista y colocar imagenes 1 hr

---
+++ src/components/CatalogComponents/ProductsList.vue
@@ -0,0 +1,59 @@
+<template>
+  <main>
+    <h2>Products</h2>
+
+    <div class="list">
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.image" alt="product image" />
+          {{ product.name }} 
+        </li>
+      </ul>
+    </div>
+    
+    
+  </main>
+
+
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        products: [
+          { id: 1, name: 'Product 1', image: 'src/components/CatalogComponents/image/auto.jpg' },
+          { id: 2, name: 'Product 2', image: 'src/components/CatalogComponents/image/auto.jpg' },
+          { id: 3, name: 'Product 3', image: 'src/components/CatalogComponents/image/auto.jpg' },
+        ],
+      };
+    },
+  };
+</script>
+<style scoped>
+
+  .list{
+    margin-left: 20%;
+    margin-right: 5%;
+    @media (max-width: 768px) {
+      margin-left: 10%;
+      margin-right: 10%;
+    }
+  }
+ 
+
+  ul {
+    list-style-type: none;
+    background-color: gray;
+    margin: 0 auto;
+  }
+
+  img{
+    max-width: 100%;
+    height: auto;
+  }
+  img{
+    width: 400px;
+    height: 400px;
+  }
+</style>
--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,5 +1,13 @@
+
+
+<script setup>
+    import ProductsList from '@/components/CatalogComponents/ProductsList.vue'
+</script>
+
 <template>
     <main>
+        <!---->
         <h1>Este es el catalogo</h1>
+        <ProductsList />
     </main>
 </template>
