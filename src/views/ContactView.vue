0b13471e84c1e9b48600668a528b053f2b7066c5
Rodrigo Dominguez Araya <rdominguez19@alumnos.utalca.cl>
2024-04-07 17:13:40 -0400

    validación y creacion de botón para formulario de contacto.Tiempo:1hr
    - Se implementó la funcionalidad para deshabilitar el botón de enviar cuando los campos no están completos.
    - Se añadió un mensaje de éxito al enviar el formulario.
    - Se agregó un mensaje de error para indicar campos faltantes en el formulario.

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,101 +1,159 @@
 <template>
-    <main>
-        <h1 class="contactoTexto">Contacto</h1>
-        <p class="textoSub">Lorem ipsum dolor sit amet consectetur adipisicing elit. A asperiores consectetur dolor error. Sed ut vero incidunt dolor asperiores enim omnis esse tempore voluptates hic, consequuntur obcaecati eos cum architecto.</p>
-        <div class="colorBox">    
-            <div class="telefonoContainer">
-                <img class="telefonoImg" src="@/assets/telefono.png" alt="Teléfono">
-                <p class="telefonoTexto">Teléfono</p>
-                <p class="textoDatos">Lorem ipsum dolor sit amet consectetur adipisicing elit. A asperiores consectetur dolor error. Sed ut vero incidunt dolor asperiores enim omnis esse tempore voluptates hic, consequuntur obcaecati eos cum architecto.</p>
-            </div>        
-            <div class="overlayBox">
-                <div>
-                    <p class="contactanos">Contáctanos</p>
-                    <input type="text" v-model="datos" placeholder="Nombre">
-                    <input type="text" v-model="datos" placeholder="Email">
-                    <textarea class="input3" v-model="campo3" placeholder="Descripción"></textarea>
-                </div>
-
-            </div>
+  <main>
+    <h1 class="contactoTexto">Contacto</h1>
+    <p class="textoSub"></p>
+    <div class="colorBox">
+      <div class="telefonoContainer">
+        <img class="telefonoImg" src="@/assets/telefono.png" alt="Teléfono" />
+        <p class="telefonoTexto">Teléfono</p>
+        <p class="textoDatos"></p>
+      </div>
+      <div class="overlayBox">
+        <div>
+          <p class="contactanos">Contáctanos</p>
+          <input type="text" v-model="datoNombre" placeholder="Nombre" @input="borrarMensaje" />
+          <input type="text" v-model="datoEmail" placeholder="Email" @input="borrarMensaje" />
+          <textarea
+            class="input3"
+            v-model="datoDescripcion"
+            placeholder="Descripción"
+            @input="borrarMensaje"
+          ></textarea>
+          <button @click="enviarFormulario" :disabled="!camposCompletos">Enviar</button>
+          <p v-if="mensajeEnviado" class="mensajeExito">Mensaje enviado correctamente</p>
+          <p v-if="mensajeNoEnviado" class="mensajeNoExito">Campos faltantes</p>
         </div>
-    </main>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style>
+<script>
+export default {
+  data() {
+    return {
+      datoNombre: '',
+      datoEmail: '',
+      datoDescripcion: '',
+      mensajeEnviado: false,
+      mensajeNoEnviado: false
+    }
+  },
+  computed: {
+    camposCompletos() {
+      return this.datoNombre && this.datoEmail && this.datoDescripcion
+    }
+  },
+  methods: {
+    enviarFormulario() {
+      console.log('Formulario enviado')
+      this.mensajeEnviado = true
+      this.mensajeNoEnviado = false
+    },
 
-.contactoTexto{
-    margin-left: 40px;
-    margin-top: 60px;
-    font-size: 50px;
-    color: #144D53;
+    borrarMensaje() {
+      this.mensajeEnviado = false
+      this.mensajeNoEnviado = true
+    }
+  }
+}
+</script>
+
+<style>
+.contactoTexto {
+  margin-left: 40px;
+  margin-top: 60px;
+  font-size: 50px;
+  color: #144d53;
 }
 
-.textoSub{
-    font-size: 15px;
-    margin: 40px
+.textoSub {
+  font-size: 15px;
+  margin: 40px;
 }
 
 .colorBox {
-    background: #307672;
-    height: 600px;
-    width: 100%;
-    margin-top: 140px;
-    margin-bottom: 140px;
+  background: #307672;
+  height: 600px;
+  width: 100%;
+  margin-top: 140px;
+  margin-bottom: 140px;
 }
 
 .overlayBox {
-    position: absolute;
-    background-color: #1A3C40;
-    height:83%;
-    width: 50%;
-    top: 35%;
-    left: 45%;
+  position: absolute;
+  background-color: #1a3c40;
+  height: 83%;
+  width: 50%;
+  top: 35%;
+  left: 45%;
 }
 
 .telefonoImg {
-    width: 40px; 
-    position: absolute; 
-    top: 54%; 
-    left: 5%;
-    transform: translate(-50%, -50%);
+  width: 40px;
+  position: absolute;
+  top: 54%;
+  left: 5%;
+  transform: translate(-50%, -50%);
 }
 
 .telefonoTexto {
-    margin-left: 130px;
-    padding-top: 45px;
-    font-size: 50px;
+  margin-left: 130px;
+  padding-top: 45px;
+  font-size: 50px;
 }
 
-.textoDatos{
-    margin-left: 80px;
+.textoDatos {
+  margin-left: 80px;
 }
 
-.contactanos{
-    text-align: center;
-    font-size: 50px;
+.contactanos {
+  text-align: center;
+  font-size: 50px;
 }
 
 .overlayBox div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 .overlayBox div input {
-    display: block;
-    margin-bottom: 50px;
-    width: 500px;
-    font-size: 20px;
+  display: block;
+  margin-bottom: 50px;
+  width: 500px;
+  font-size: 20px;
 }
 
-.input3{
-    display: block;
-    margin-bottom: 50px;
-    width: 500px;
-    height: 280px;
-    font-size: 20px;
-    resize: none;
+.input3 {
+  display: block;
+  margin-bottom: 50px;
+  width: 500px;
+  height: 280px;
+  font-size: 20px;
+  resize: none;
+}
+button {
+  background-color: #307672;
+  color: white;
+  padding: 1rem;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  margin-top: 2rem;
+  font-size: 20px;
 }
 
+button:hover {
+  background-color: #1d4e4a;
+}
+
+button:active {
+  background-color: #1d4e4a;
+}
+
+button:focus {
+  outline: none;
+}
 </style>
