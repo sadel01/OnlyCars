ba365c590a82dbe6db315413f4899f53075dedfc
Jose Diaz <josdiaz21@alumnos.utalca.cl>
2024-04-07 22:37:52 -0400

    Ajustes Navbar y pagina principal: 1 hora

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -23,7 +23,13 @@ defineProps({
 
         <div class="botoncito">
           <RouterLink to="/catalog">
-          <button>Ver Catalogo</button>
+            <button>
+    <span>Ver Catalogo</span>
+    <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
+        <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
+    </svg>
+</button>
           </RouterLink>
         </div>
     </main>
@@ -41,6 +47,7 @@ defineProps({
 }
 
 .botoncito{
+
   position: absolute;
   top: 430px; /*570*/
   left: 870px; /*1620*/
@@ -98,25 +105,37 @@ main {
 }
 
 button {
-  background-color: #307672;
-  color: white;
-  padding: 1rem;
-  border: none;
-  border-radius: 8px;
   cursor: pointer;
-  margin-top: 2rem;
-  font-size: 20px;
+  font-weight: 700;
+  font-family: Helvetica,"sans-serif";
+  transition: all .2s;
+  padding: 10px 20px;
+  border-radius: 100px;
+  background: #FBC40E;
+  border: 1px solid transparent;
+  display: flex;
+  align-items: center;
+  font-size: 15px;
 }
 
 button:hover {
-  background-color: #1d4e4a;
+  background: #FBC40E;
+}
+
+button > svg {
+  width: 34px;
+  margin-left: 10px;
+  transition: transform .3s ease-in-out;
+}
+
+button:hover svg {
+  transform: translateX(5px);
 }
 
 button:active {
-  background-color: #1d4e4a;
+  transform: scale(0.95);
 }
 
-button:focus {
-  outline: none;
-}
+
+
 </style>
