a654765ea82ce7b232ad11bb188cae22524e8858
Nelson Silva <nelsonsjf@gmail.com>
2024-04-09 23:32:05 -0400

    Conexión /login y /register con DB. Tiempo: 40min

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -7,20 +7,41 @@ const app = express();
 const url = `mongodb+srv://admin:12345adminADMIN@cluster0.2sd1gmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(url);
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello' });
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db('onlycars');
+        const collection = database.collection('users');
+        const result = await collection.insertOne(req.body);
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-app.get('/catalog', (request, response) => {
-    response.json(carItems);
+app.post('/login', async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db('onlycars');
+        const collection = database.collection('users');
+        const user = await collection.findOne({email: req.body.email});
+        if (user) {
+            if (user.password === req.body.password) {
+                res.send('Login successful');
+            } else {
+                res.send('Incorrect password');
+            }
+        }else{
+            res.send('User not found');
+        }
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-app.get('/catalog/:id', (request, response) => {
-    const carID = request.params.id;
-    const car = carItems.find((car) => car.id == carID);
-    return response.json(car);
-});
 
-app.listen(5174, () => {
-  console.log('Server started on port 5174!');
+app.listen(5173, () => {
+  console.log('Server started on port 5173!');
 });
