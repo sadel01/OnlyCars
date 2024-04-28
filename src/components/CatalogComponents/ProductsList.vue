07362946b3feb264f43ca863b0a0e2a669bb28ed
Benjamín Puebla <bpuebla21@alumnos.utalca.cl>
2024-04-28 19:40:29 -0400

    Botón de Favorito en catálogo. Tiempo 30 min
    Ultimo commit previo a la semana de estudio autonomo

--- src/components/CatalogComponents/ProductsList.vue
+++ src/components/CatalogComponents/ProductsList.vue
@@ -60,6 +60,21 @@
                 </div>
                 <button @click="viewMore(product._id)" class="verMas2"><span>Ver más</span></button>
               </div>
+
+              <label class="containerFav" @click.stop="addToFavorites(product)">
+                <input type="checkbox" />
+                <div class="checkmark">
+                  <svg viewBox="0 0 256 256">
+                    <rect fill="none" height="256" width="256"></rect>
+                    <path
+                      d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                      stroke-width="20px"
+                      stroke="black"
+                      fill="none"
+                    ></path>
+                  </svg>
+                </div>
+              </label>
             </div>
           </li>
         </ul>
@@ -115,6 +130,9 @@ export default {
     }
   },
   methods: {
+    addToFavorites(product) {
+      //Método para agregar los vehiculos a favoritos
+    },
     goToComparisonView() {
       this.$store.commit('comparison/setList', this.comparisonList)
       this.$router.push({ name: 'comparison' })
@@ -224,6 +242,75 @@ export default {
 </script>
 
 <style scoped>
+
+.containerFav input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+.containerFav {
+  display: block;
+  position: relative;
+  right: 2%;
+  cursor: pointer;
+  font-size: 20px;
+  user-select: none;
+  transition: 100ms;
+}
+
+.checkmark {
+  top: 0;
+  left: 0;
+  height: 2em;
+  width: 2em;
+  transition: 100ms;
+  animation: dislike_effect 400ms ease;
+}
+
+.containerFav input:checked ~ .checkmark path {
+  fill: #fbc40e;
+  stroke-width: 0;
+}
+
+.containerFav input:checked ~ .checkmark {
+  animation: like_effect 400ms ease;
+}
+
+.containerFav:hover {
+  transform: scale(1.1);
+}
+
+@keyframes like_effect {
+  0% {
+    transform: scale(0);
+  }
+
+  50% {
+    transform: scale(1.2);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes dislike_effect {
+  0% {
+    transform: scale(0);
+  }
+
+  50% {
+    transform: scale(1.2);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+
 .detail button {
   border: none;
   color: white;
